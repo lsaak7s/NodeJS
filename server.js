840b60a3c54801116aa4b12e5d5d170d1dc50690
Isaac Alves <Isaacsantos.7r@gmail.com>
2026-08-25 16:38:57 -0300

    Nodejs

--- server.js
+++ server.js
@@ -1,32 +1,31 @@
-//isaacsantos7r_db_user
-//ce6BPLrzGIin7jWL
+/*Senhas do mongodb
+isaacsantos7r_db_user
+ce6BPLrzGIin7jWL*/
+
 //start from scratch
+
 //Detendo conhecimento Nodejs
 
-//Aqui eu estou importando a biblioteca no caso a express
-
+//Aqui eu estou importando as bibliotecas
 import { PrismaClient } from "./generated/prisma/client.js";
 import express from "express";
-
-//app é a nossa aplicação Express. É através dela que vamos criar as rotas, como GET e POST, e configurar o servidor.
-const app = express();
-const prisma = new PrismaClient();
-//Estamos guardando os nossos usuarios
 
 //Temos que avisar para o express que estamos usando json se não ele não mostra o body
 app.use(express.json());
 
-//`:` diz que criamos uma variavel , porque sempre precisamos da variavel para guardar um valor
+//Aqui estamos colocando nossas Frameorks em variaveis
+const app = express();
+const prisma = new PrismaClient();
 
 //cria uma rota GET, que apenas busca a lista de usuarios
 app.get("/usuarios", async (req, res) => {
-    //Estamos mandnado a informação que ja temos
+
     const users = await prisma.user.findMany()
 
     res.status(200).json(users);
 });
-//cria uma rota POST
 
+//cria uma rota POST que cria usuarios
 app.post('/usuarios', async (req, res) => {
     await prisma.user.create({
         data: {
@@ -38,7 +37,7 @@ app.post('/usuarios', async (req, res) => {
 
     res.status(201).json({ mensagem: "Usuario Criado com sucesso" });
 })
-
+//A rota PUT altera os usuarios
 app.put('/usuarios/:id', async (req, res) => {
 
     const user = await prisma.user.update({
@@ -54,7 +53,7 @@ app.put('/usuarios/:id', async (req, res) => {
 
     res.status(201).json({ mensagem: "Usuario Alterado com sucesso" });
 })
-
+//A rota delete deleta os usuarios
 app.delete('/usuarios/:id', async (req, res) => {
 
     const user = await prisma.user.delete({
@@ -65,5 +64,6 @@ app.delete('/usuarios/:id', async (req, res) => {
 
     res.status(201).json({ mensagem: "Usuario Deletado com sucesso" });
 })
+
 //coloca o servidor para "escutar" na porta 3000.
 app.listen(3000);
